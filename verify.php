30bc81b1d59029408a63c7f61481d23be74b4679
nick2meetyou <n.thansaya@gmail.com>
2018-03-21 23:02:43 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -7,7 +7,7 @@
     ];
   $data = [
     'replyToken' => $access_token,
-    'messages' = > [$messages]
+    'messages' => [$messages]
     ];
   $post = json_encode($data);
   $headers = array('Content-Type': application/json','Authorization: Bearer ' . $access_token);
