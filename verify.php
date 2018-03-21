30ce4570a0b5de2d4b6bd2551621cafe0d95517a
nick2meetyou <n.thansaya@gmail.com>
2018-03-21 22:43:50 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,6 @@
 <?php
   $access_token = 'vINbFh21yiQmlEokfzp8mBvY0XekEe84XZb0nNAz9Gs5CwVcUE62hGJQctcbytu1OAQZMacGDXb0jx5cC7aLywrijb4XwNXvVv2aWVzkqHaI4e6Uuy02v++p+uM8bKXiXu0qgUH3u/1LeNRuLppkBAdB04t89/1O/w1cDnyilFU=';
-  $url = 'https://api.line.me/v1/oauth/verify';
+  $url = 'https://api.line.me/v2/oauth/verify';
   $headers = array('Authorization: Bearer ' . $access_token);
   $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
