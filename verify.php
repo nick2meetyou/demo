cb69672cc4f4a5980ef79c4ee86264329b44ca15
nick2meetyou <n.thansaya@gmail.com>
2018-03-22 15:14:03 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-  $access_token = 'vINbFh21yiQmlEokfzp8mBvY0XekEe84XZb0nNAz9Gs5CwVcUE62hGJQctcbytu1OAQZMacGDXb0jx5cC7aLywrijb4XwNXvVv2aWVzkqHaI4e6Uuy02v++p+uM8bKXiXu0qgUH3u/1LeNRuLppkBAdB04t89/1O/w1cDnyilFU=';
+  $access_token = 'NE8U+tIMQayHvkZ4JpIjVAhld8vu5RheOKdZHkYkkzJZP6MTaZqjf7bOqL4nD19kOAQZMacGDXb0jx5cC7aLywrijb4XwNXvVv2aWVzkqHbP2tjox8xVTde8qMMDzek+dNgaVs+w2pmLveeFn07tcAdB04t89/1O/w1cDnyilFU=';
   $url = 'https://api.line.me/oauth2/v2.1/verify';
   $headers = array('Authorization: Bearer ' . $access_token);
   $ch = curl_init($url);
