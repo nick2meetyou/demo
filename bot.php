999525ff957e9f3004fc9f7bda0153eed16c7d0c
nick2meetyou <n.thansaya@gmail.com>
2018-03-22 22:42:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,52 @@
 <?php
-  echo "Hi I'm bot"
-?>
+  $API_URL = 'https://api.line.me/v2/bot/message/reply';
+  $ACCESS_TOKEN = 'pfE4jg1PSrT1mBnuNgkvnic4dMYduwPtrAtKnDM9mOqC/PrwZLuYvZTaRGOGurMGOAQZMacGDXb0jx5cC7aLywrijb4XwNXvVv2aWVzkqHY/1jIpv0MJsQYaZLBQUuk//+C9DQBku7KkBSgGzgkrVgdB04t89/1O/w1cDnyilFU=';
+  $POST_HEADER = array('Content-Type: application/json','Authorization: Bearer ' . $ACCESS_TOKEN);
+
+  $request = file_get_contents('php://input');
+  $request_array = json_decode($request, true);
+
+  if(sizeof($request_array['events']) > 0){
+    foreach ($request_array['events'] as $event) {
+      if( $event['type'] == 'message'){
+        if($event['message']['type'] == 'text'){
+          $text = $event['message']['text'];
+          $reply_message = 'รักที่รักที่สุดเลย';
+        }
+        else
+       $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
+
+     }
+     else
+      $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
+
+     if( strlen($reply_message) > 0 )
+     {
+      //$reply_message = iconv("tis-620","utf-8",$reply_message);
+      $data = [
+       'replyToken' => $reply_token,
+       'messages' => [['type' => 'text', 'text' => $reply_message]]
+      ];
+      $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+      $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
+      echo "Result: ".$send_result."\r\n";
+     }
+    }
+   }
+
+   echo "OK";
+
+   function send_reply_message($url, $post_header, $post_body)
+   {
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+   }
