84f46dbd2fb52da5cafca85d45807ddd524379ae
nick2meetyou <n.thansaya@gmail.com>
2018-03-22 15:03:10 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,16 +1,9 @@
 <?php
   $access_token = 'vINbFh21yiQmlEokfzp8mBvY0XekEe84XZb0nNAz9Gs5CwVcUE62hGJQctcbytu1OAQZMacGDXb0jx5cC7aLywrijb4XwNXvVv2aWVzkqHaI4e6Uuy02v++p+uM8bKXiXu0qgUH3u/1LeNRuLppkBAdB04t89/1O/w1cDnyilFU=';
   $url = 'https://api.line.me/oauth2/v2.1/verify';
-  $messages = [
-    'type' => 'text',
-    'text' => 'Hi We are Sabotenstore'
-    ];
-  //$post = json_encode($data);
-  $headers = array('Content-Type : application/json','Authorization: Bearer ' . $access_token);
+  $headers = array('Authorization: Bearer ' . $access_token);
   $ch = curl_init($url);
-  //curl_setopt($ch, CURLOPT_CUSTOMREQUEST,"POST");
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//  curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
   curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
   $result = curl_exec($ch);
